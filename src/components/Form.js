c4f9b5c2fd4d49de630056768b53902d4091530d
Kirill <kirill.tovpinets@gmail.com>
2018-07-13 13:32:49 +0300

    a complete styled application

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+const placesAPI = 'AIzaSyB_qKihkMk6qBzNenSu0I3Pp0C8cdlXLcE';
 export default class Form extends React.Component{
     render(){
         return(
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="city" class="form-control"/>
-                <input type="text" name="country" placeholder="country" class="form-control"/>
-                <button class="btn btn-success">Get wheather</button>
+                <div className="form-group">
+                    <input type="text" name="city" placeholder="city" className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <input type="text" name="country" placeholder="country" className="form-control"/>
+                </div>
+                <button className="btn btn-success">Get wheather</button>
             </form>
         );
     }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,11 +4,14 @@ export default class Weather extends React.Component{
     render(){
         return(
             <div className="weather-output">
-                {this.props.temperature && <p>{this.props.temperature}</p>}
-                {this.props.city && <p>{this.props.city}</p>}
-                {this.props.country && <p>{this.props.country}</p>}
-                {this.props.humidity && <p>{this.props.humidity}</p>}
-                {this.props.description && <p>{this.props.description}</p>}
+                <div className="content">
+                    {!this.props.temperature && <h1>Please, choose location</h1>}
+                    {this.props.temperature && <p>Location: {this.props.temperature}</p>}
+                    {this.props.city && <p>City: {this.props.city}</p>}
+                    {this.props.country && <p>Country: {this.props.country}</p>}
+                    {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                    {this.props.description && <p>Description: {this.props.description}</p>}
+                </div>
             </div>
         )
     }
